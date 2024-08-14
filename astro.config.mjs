6a00e942f81da952c9c6ba667614d040604c6939
aviv-rs <aviv80010@gmail.com>
2024-08-14 15:59:06 +0300

    added additional settings to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+const SERVER_PORT = 4321;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "https://aviv-rs.github.io/My-portfolio";
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
-  site: 'https://aviv-rs.github.io/My-portfolio',
-  base: 'My-portfolio',
+  site:  BASE_URL,
 })
